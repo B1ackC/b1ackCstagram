ef1dde2be18183571cd8b6653593a4f669e54cce
B1ackC <owlx113@gmail.com>
2024-06-03 22:47:28 +0900

    Change-GET/POST

--- my_falsk_project/app.py
+++ my_falsk_project/app.py
@@ -1,23 +1,15 @@
 from flask import Flask, render_template, request, redirect
 import random
 import os
+# from src.data import post
+# from src.data.post import Post
+# from src.store.store import Store
 
 app = Flask(__name__, static_folder='static')
-
 #전역변수
 users = []
 posts = []
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-
-class Board:
-    def __init__(self):
-        self.posts = posts
-
-    def search_post(self, search_term):
-        global posts
-        if search_term:
-            return [post for post in self.posts if search_term in post['title']]
-        return self.posts
 
 
 #파일 읽기 
@@ -39,7 +31,7 @@ def allow_file(filename):
         filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 #랜덤 이미지 출력
-@app.route('/main')
+@app.get('/main')
 def main():
     texts = load_texts('static/texts.txt')
     random_text = random.choice(texts)
@@ -47,63 +39,58 @@ def main():
     return render_template('main.html', random_text=random_text, image_file=f"image/{random_image}")
 
 #이미지 1~5
-@app.route('/random/<int:image_id>')
+@app.get('/random/<int:image_id>')
 def random_image(image_id):
     image_file = f"image/{image_id}.png"
     texts = load_texts('static/texts.txt')
     random_text = texts[image_id - 1]
     return render_template('main.html', random_text=random_text, image_file=image_file)
 
-@app.route('/', methods=['GET', 'POST'])
-def userLogin():
-    # TODO: 로그인 기능 구현
-    if request.method == 'GET':
-        return render_template("login.html")
-    else:
-        userId = request.form.get("userId")
-        userPassword = request.form.get('userPassword')
-        # user id, password 확인 후 로그인
-        for user in users:
-            if user['userId'] == userId and user['userPassword'] == userPassword:
-                return redirect('/main')
-        return redirect('/')
+#로그인
+@app.get('/')
+def getLogin():
+    return render_template('login.html')
 
+@app.post('/')
+def postLogin():
+    userId = request.form.get('userId')
+    userPassword = request.form.get('userPassword')
+    for user in users:
+        if user['userId'] == userId and user['userPassword'] == userPassword:
+            return redirect('/main')
+    return redirect('/')
 
-@app.route('/register', methods=['GET','POST'])
-def registerUser():
-    if request.method == 'GET':
-        return render_template("join.html")
-    else:
-        userId = request.form.get('userId')
-        userPassword = request.form.get('userPassword')
-        userName = request.form.get('userName')
-        # 값 받아오는지 확인
-        users.append({
-             'userId': userId,
-             'userPassword': userPassword,
-             'userName': userName
-        })
+#회원가입
+@app.get('/register')
+def getRegister():
+    return render_template('join.html')
 
-        print(users)
-        # 추후 DB 연동
-        return redirect('/post')
-    
-@app.route('/post', methods=['GET', 'POST'])
-def postContent():
-    #전역변수 사용 선언
-    global posts
-    if request.method == 'GET':
-        return render_template('post.html')
-    #값 입력시 저장
-    elif request.method == 'POST':
-        # contentNumber = len(posts) + 1
+@app.post('/register')
+def postRegister():
+    userId = request.form.get('userId')
+    userPassword = request.form.get('userPassword')
+    userName = request.form.get('userName')
+    # 값 받아오는지 확인
+    users.append({
+        'userId': userId,
+        'userPassword': userPassword,
+        'userName': userName
+    })
+    return redirect('/post')
+
+#게시글 작성
+@app.get('/post')
+def getPosts():
+    return render_template('post.html')
+
+@app.post('/post')
+def insertPosts():
+        global posts
         contentTitle = request.form.get('title')
         contentText = request.form.get('content')
         imageFile = request.files['file']
-
+        
         if imageFile and allow_file(imageFile.filename):
-        # if imageFile:
-            # filename = imageFile.filename
             filename = imageFile.filename
             filepath = os.path.join('static/upload', imageFile.filename)
             imageFile.save(filepath)
@@ -111,30 +98,26 @@ def postContent():
             filename = None
         # 배열 데이터 삽입
         posts.append({
-            'number': len(posts) + 1,  # 클래스 내부 변수 self.posts 사용
+            'number': len(posts) + 1,
             'title': contentTitle,
             'context': contentText,
             'fileName': filename
         })
-        # Board.add_post(contentTitle, contentText, filename)
         return redirect('/board')
-    else:
-        return redirect('/post')
 
-@app.route('/board', methods=['GET', 'POST'])
-def board():
-    if request.method == 'GET':
-        board = Board()
-        return render_template('board.html', posts=posts)
-    if request.method == 'POST':
-        # filter_posts = []
-        search_term = request.form.get('search')
-        print(search_term)
-        a = Board()
-        # filter_posts = a.search_post(search_term)
-        print(a.search_post(search_term))
-        return render_template('board.html', posts=a.search_post(search_term), search_term=search_term)
+#게시글 보기
+@app.get('/board')
+def getBoard():
+    return render_template('board.html', posts=posts)
 
+@app.post('/board')
+def searchPost():
+    global posts
+    search_term = request.form.get('search')
+    if search_term:
+        filtered_posts = [post for post in posts if search_term in post['title']]
+        return render_template('board.html', posts=filtered_posts)
+    return render_template('board.html', posts=posts)
 
 
 if __name__ == '__main__':
